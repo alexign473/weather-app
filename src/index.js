389ce7ad6dea08a939a020f4b4e46f238c4c414a
Alexander Ignatev <alexign473@gmail.com>
2023-04-10 22:28:43 +0300

    Api call

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 import './style.scss';
 
-console.log('Hello');
+const getData = async (query) => {
+  try {
+    const response = await fetch(
+      `https://api.weatherapi.com/v1/current.json?key=3babf595b26c4b1482c111127231004&q=${query}`,
+      { mode: 'cors' }
+    );
+    const data = await response.json();
+    console.log(data);
+
+    if (!response.ok) {
+      throw Error(data.error.message);
+    }
+
+    return data;
+  } catch (err) {
+    console.warn(err);
+  }
+};
+
+getData('london');
